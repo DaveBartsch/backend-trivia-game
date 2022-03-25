ee469e26b0aaa866f4a36d4c24c9fc122c3dc8ee
David <dave.bartsch@icloud.com>
2022-03-25 12:55:17 -0600

    final commit

--- functions.js
+++ functions.js
@@ -1,10 +1,8 @@
 const { qArray } = require("./questionsArray");
 
-
 const gameState = {
   score: 0,
   questionNum: 0,
-  
 };
 
 const youAreWrong = "Wrong answer! <br>";
@@ -28,14 +26,14 @@ const shuffle = (array) => {
     ];
   }
   return array;
-}
+};
 
 //randomized choices from 0-3
 const choiceArray = [0, 1, 2, 3];
 let randomOrder = shuffle(choiceArray);
 // console.log(`The random order is: ${randomOrder}`);
 
-//creates an array of possible URLs for the user to visit
+//creates an array of 4 possible URLs
 const turnIntoURL = (questionObj) => {
   return [
     `<a href ="http://localhost:3000/trivia?answer=${questionObj.correct}">${questionObj.correct}</a>`,
@@ -43,48 +41,60 @@ const turnIntoURL = (questionObj) => {
     `<a href ="http://localhost:3000/trivia?answer=${questionObj.wrong2}">${questionObj.wrong2}</a>`,
     `<a href ="http://localhost:3000/trivia?answer=${questionObj.wrong3}">${questionObj.wrong3}</a>`,
   ];
-}
+};
 
 //structures the URLs, in a random order
 const askQuestion = (questionObj) => {
   // console.log(`questionObj is: ${questionObj}`)
   URLs = turnIntoURL(questionObj);
   return (
-    questionObj.q + "<br><br>" +
-    URLs[randomOrder[0]] + "<br>" +
-    URLs[randomOrder[1]] + "<br>" +
-    URLs[randomOrder[2]] + "<br>" +
-    URLs[randomOrder[3]] + "<br>"
+    questionObj.q +
+    "<br><br>" +
+    URLs[randomOrder[0]] +
+    "<br>" +
+    URLs[randomOrder[1]] +
+    "<br>" +
+    URLs[randomOrder[2]] +
+    "<br>" +
+    URLs[randomOrder[3]] +
+    "<br>"
   );
-}
+};
 
 //determines whether the answer query is a correct answer or a wrong answer
 const gradeAnswer = (questionObj, answer) => {
-//  console.log(`questionObj is: ${JSON.stringify(questionObj)}`)
- randomOrder = shuffle(choiceArray);
+  //  console.log(`questionObj is: ${JSON.stringify(questionObj)}`)
+  randomOrder = shuffle(choiceArray);
 
-if (answer === qArray[gameState.questionNum].correct) {
-  gameState.questionNum = gameState.questionNum +1;
+  //if answer is correct choice for the current question, add 1 to score, add 1 to question number
+  if (answer === qArray[gameState.questionNum].correct) {
+    gameState.questionNum = gameState.questionNum + 1;
     gameState.score = gameState.score + 1;
     // console.log(`The random order is: ${randomOrder}`);
-    console.log(`score is ${gameState.score}, question # is: ${gameState.questionNum}`)
+    console.log(
+      `score is ${gameState.score}, question # is: ${gameState.questionNum}`
+    );
     return youAreRight + askQuestion(qArray[gameState.questionNum]);
+    //if answer is a wrong choice for the current question, add 1 to the question number
   } else if (
     answer === qArray[gameState.questionNum].wrong1 ||
     answer === qArray[gameState.questionNum].wrong2 ||
-    answer === qArray[gameState.questionNum].wrong3  
-    ) {
-      gameState.questionNum = gameState.questionNum +1;
-      // console.log(`The random order is: ${randomOrder}`);
-      console.log(`score is ${gameState.score}, question # is: ${gameState.questionNum}`)
-      return youAreWrong + askQuestion(qArray[gameState.questionNum]);
-  }  
-else {
-  console.log(`User has an Error! The question # was (${gameState.questionNum}). The score was (${gameState.score})`)
+    answer === qArray[gameState.questionNum].wrong3
+  ) {
+    gameState.questionNum = gameState.questionNum + 1;
+    // console.log(`The random order is: ${randomOrder}`);
+    console.log(
+      `score is ${gameState.score}, question # is: ${gameState.questionNum}`
+    );
+    return youAreWrong + askQuestion(qArray[gameState.questionNum]);
+    //if anything else, display an error message, redirect user to Start page
+  } else {
+    console.log(
+      `User has an Error! The question # was (${gameState.questionNum}). The score was (${gameState.score})`
+    );
     return "Invalid input! Go back and try again! Or go back to the beginning to start over: <br> <a href = 'http://localhost:3000/start'>Start Over</a>";
   }
-}
-
+};
 
 module.exports = {
   youAreRight,
